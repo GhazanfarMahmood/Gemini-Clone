3ac92ef5d1108acc7953a422796f860c8da6270c
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2024-08-04 08:38:55 +0500

    Config the gemini api

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,58 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import './SideBar.css'
+import "./SideBar.css";
 
-import {assets} from '../../assets/assets'
-
+import { assets } from "../../assets/assets";
 
 const SideBar = () => {
-
-    const [extended, setExtended] = useState(true);
-
-    const handleClose = () =>{
-        if(extended === true){
-            setExtended(false)
-        }else{
-            setExtended(true)
-        }
-    }
-
+  const [extended, setExtended] = useState(true);
 
   return (
-    <div className='sidebar'>
-        <div className="top">
-            <img className='menu' src={assets.menu_icon} alt="" onClick={handleClose}/>
+    <div className="sidebar">
+      <div className="top">
+        <img
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+          onClick={() => {
+            extended === true ? setExtended(false) : setExtended(true);
+          }}
+        />
 
-            <div className="new-chat">
-                <img src={assets.plus_icon} alt="" />
-                {!extended &&<p>New Chat</p> }
-            </div>
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {!extended && <p>New Chat</p>}
+        </div>
 
-           {!extended && <div className="recent">
-                <p className='recent-title'>Recent</p>
-                <div className="recent-entry">
-                    <img src={assets.message_icon} alt="" />
-                    <p>What is react...</p>
-                </div>
+        {!extended && (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react...</p>
             </div>
-            }
+          </div>
+        )}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {!extended && <p>Help</p>}
         </div>
-        <div className="bottom">
-            <div className="bottom-item recent-entry">
-                <img src={assets.question_icon} alt="" />
-                {!extended &&<p>Help</p>}
-            </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.history_icon} alt="" />
-                {!extended && <p>Activity</p>}
-            </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.setting_icon} alt="" />
-                {!extended &&<p>Setting</p>}
-            </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {!extended && <p>Activity</p>}
         </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {!extended && <p>Setting</p>}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
